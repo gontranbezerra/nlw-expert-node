d0981179cac585b67c03457fc74480d4e1dcffd7
Gontran Bezerra <gontranbezerra@gmail.com>
2024-02-11 07:41:58 -0300

    feat: Implementing polls route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,19 +1,28 @@
-import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+import fastify, { FastifyReply, FastifyRequest } from 'fastify';
 
 import z from 'zod';
 
 const app = fastify();
+const prisma = new PrismaClient();
 
 // GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
 
-app.post('/polls', (request) => {
+app.post('/polls', async (request: FastifyRequest, reply: FastifyReply) => {
   const createPollBody = z.object({
     title: z.string(),
   });
   const { title } = createPollBody.parse(request.body);
 
-  console.log(title);
-  return 'Teste ok!';
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  //   return { pollId: poll.id };
+  return reply.status(201).send({ pollId: poll.id });
 });
 
 app.listen({ port: 3333 }).then(() => {
