63cebae729a1d34f0a4bc9465f3fdb7d51215e39
Gontran Bezerra <gontranbezerra@gmail.com>
2024-02-11 07:27:35 -0300

    config: Installing zod

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,20 @@
 import fastify from 'fastify';
 
+import z from 'zod';
+
 const app = fastify();
+
+// GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
+
+app.post('/polls', (request) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+  const { title } = createPollBody.parse(request.body);
+
+  console.log(title);
+  return 'Teste ok!';
+});
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!');
