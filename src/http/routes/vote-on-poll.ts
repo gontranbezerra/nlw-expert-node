71921afbb178fb0dc04c557cbafff34b7c2528f5
Gontran Bezerra <gontranbezerra@gmail.com>
2024-02-11 10:02:38 -0300

    feat: Implementing vote on poll route

---
+++ src/http/routes/vote-on-poll.ts
@@ -0,0 +1,69 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+
+import z from 'zod';
+
+import { randomUUID } from 'node:crypto';
+
+import { prisma } from '../../lib/prisma';
+
+export async function voteOnPoll(app: FastifyInstance) {
+  app.post('/polls/:pollId/votes', async (request: FastifyRequest, reply: FastifyReply) => {
+    const voteOnPollParams = z.object({
+      pollId: z.string().uuid(),
+    });
+
+    const voteOnPollBody = z.object({
+      pollOptionId: z.string().uuid(),
+    });
+
+    const { pollId } = voteOnPollParams.parse(request.params);
+    const { pollOptionId } = voteOnPollBody.parse(request.body);
+
+    // Criando uma identificação só para exercicio.
+    // Melhor abordagem é via login
+    let { sessionId } = request.cookies;
+
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId,
+          },
+        },
+      });
+
+      if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id
+          }
+        })
+      } else if (userPreviousVoteOnPoll) {
+        return reply.status(400).send({ message: 'You already voted on this poll.' });
+      }
+    }
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    const vote = await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId,
+      },
+    });
+
+    //   return { pollId: poll.id };
+    return reply.status(201).send({ vote: true });
+  });
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,14 +1,23 @@
+import cookie, { FastifyCookieOptions } from '@fastify/cookie';
+
 import fastify from 'fastify';
 
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
+import { voteOnPoll } from './routes/vote-on-poll';
 
 const app = fastify();
 
-// GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
+app.register(cookie, {
+  secret: 'my-secret-polls-app-nlw-XPTO', // for cookies signature - for cookie idintify
+  hook: 'onRequest', // set to false to disable cookie autoparsing or set autoparsing on any of the following hooks: 'onRequest', 'preParsing', 'preHandler', 'preValidation'. default: 'onRequest'
+  //   parseOptions: {}, // options for parsing cookies
+} as FastifyCookieOptions);
 
+// Routes: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!');
